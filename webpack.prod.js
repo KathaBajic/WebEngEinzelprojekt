e7b54279ed5e6a82bfc6a7230c505c009ba713f4
KathaBajic <katharinabajic@gmail.com>
2021-09-21 17:12:48 +0200

    changed the structure of the webpack files and the overall structure of the project

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,72 +1,10 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); 
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-module.exports = {
-  mode: 'production',
-  // 1
-  // Use the src/index.js file as entry point to bundle it.
-  // If the src/index.js file imports other JS files,
-  // bundle them as well
-  entry: path.resolve(__dirname, './src/index.js'),
-  // 2
-  // The bundles source code files shall result in a bundle.js file
-  // in the /dist folder
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js',
-    library: 'EntryPoint'
-  },
-  // 3
-  // The /dist folder will be used to serve our application
-  // to the browser
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
-    static: path.resolve(__dirname, './dist'),
+    static: './dist',
   },
-  // 4
-  // Add plugins for webpack here
-  plugins: [
-    new CleanWebpackPlugin,
-    new HtmlWebpackPlugin({
-      title: "Web Eng Einzelprojekt Katharina Bajic",
-      template: path.resolve(__dirname, './src/index.html'),
-    }),
-    new HtmlWebpackPlugin({
-      title: "Kontakt",
-      filename: "kontakt.html",
-      template: path.resolve(__dirname, './src/kontakt.html'),
-    }),
-    new HtmlWebpackPlugin({
-      title: "Liste",
-      filename: "liste.html",
-      template: path.resolve(__dirname, './src/liste.html'),
-    }),
-  ],
-  // 5 
-  // Integrate Babel in the build process
-  // Define which files to use the loader
-  module: {
-    // configuration regarding modules
-    rules: [
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/, // files to exclude
-        use: ['babel-loader', 'eslint-loader'],
-      },
-      // CSS and SASS
-      {
-        test: /\.(scss|css)$/, // load files that end with scss and css
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-    ],
-  },
-  resolve: {
-    // options for resolving module requests
-    extensions: ['*', '.js']  // files to load
-  },
-  devtool: "source-map"
-};
+});
