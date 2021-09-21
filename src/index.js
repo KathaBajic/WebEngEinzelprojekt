0ecc017d1c4f8270a16895b4e585c3a65194e62b
KathaBajic <katharinabajic@gmail.com>
2021-09-21 18:04:36 +0200

    added event listener to forms and add alert for success and error

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 import './styles/style.scss';
 
+let initHome = function () {
+    let checkbtn = document.getElementById("submitbtn");
+    checkbtn.addEventListener("click", function () {
+        validateInput(this);
+    })
+}
+
+let validateInput = function() {
+    let validity = (
+        document.getElementById("fname").checkValidity() &&
+        document.getElementById("lname").checkValidity() &&
+        document.getElementById("age").checkValidity() &&
+        document.getElementById("email").checkValidity() &&
+        document.getElementById("pass").checkValidity()
+    );
+    if (validity) {
+        alert('Success');
+    } else {
+        alert('Error');
+    }
+}
+
+window.initHome = initHome;
